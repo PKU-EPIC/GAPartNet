478dc864aca92c35a4d085987cdf04d08c4d5cc5
geng-haoran <2000012975@stu.pku.edu.cn>
2023-10-26 18:21:45 +0800

    new

--- gapartnet/train.sh
+++ gapartnet/train.sh
@@ -1,15 +1,19 @@
-CUDA_VISIBLE_DEVICES=7 \
+# training with debug mode: without wandb logging
+CUDA_VISIBLE_DEVICES=0 \
 python train.py fit -c gapartnet.yaml \
+--model.init_args.training_schedule "[5,10]" \
 --model.init_args.ckpt ckpt/release.ckpt \
 --model.init_args.debug True
 
-
-CUDA_VISIBLE_DEVICES=5 \
+# test
+CUDA_VISIBLE_DEVICES=0 \
 python train.py test -c gapartnet.yaml \
 --model.init_args.training_schedule "[0,0]" \
 --model.init_args.ckpt ckpt/release.ckpt
 
+# training
 CUDA_VISIBLE_DEVICES=0 \
 python train.py fit -c gapartnet.yaml \
+--model.init_args.training_schedule "[5,10]" \
 --model.init_args.ckpt ckpt/release.ckpt
 
